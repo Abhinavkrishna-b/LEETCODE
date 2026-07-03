ef7f77171834d1d9863e34c8d24407e6c9f06968
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-07-03 14:15:13 +0530

    Time: 0 ms (100%), Space: 42 MB (51.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,21 @@
 class Solution {
     public int fib(int n) {
-        //Brute force solution with no DP
-        //This solution contains overlapping subproblems
-        if(n == 0)  return 0;
-        if(n == 1 || n == 2)    return 1;
-        return fib(n-1)+fib(n-2);
+        //DP approach - Top Down (Memoization)
+        int memo[] = new int[n+1];
+        return fibanocci(n,memo);
+    }
+
+    public int fibanocci(int n,int memo[]){
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        if(memo[n] != 0){
+            return memo[n];
+        }
+
+        memo[n] = fibanocci(n-1, memo) + fibanocci(n-2, memo);
+        return memo[n];
     }
 }
-//Time- O(2^n)
-//Space- O(1)
+//Time- O(n)
+//Space- O(n)
