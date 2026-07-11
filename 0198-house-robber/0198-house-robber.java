d704e06ad5cd0e232099b63bbd8d7b8a2aa068e8
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-07-11 09:14:04 +0530

    Time: 0 ms (100%), Space: 43 MB (18.46%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,19 @@
+class Solution {
+    //Recursion with overlapping subproblems
+    public int rob(int[] nums) {
+        // [2,1,1,2] it is not necessary that you need to rob any one house that is adjacent - here to get max money you not rob 1 or 1 instead you go to 2 and 2
+        int dp[] = new int[nums.length];
+        Arrays.fill(dp,-1);
+        return solve(0,nums,dp);
+    }
+
+    public int solve(int i, int nums[],int dp[]){
+        if(i >= nums.length)    return 0;
+        if(dp[i] != -1)    return dp[i];
+
+        dp[i] = Math.max(nums[i]+solve(i+2,nums,dp), solve(i+1,nums,dp));
+        return dp[i];
+    }
+}
+//Time- O(n)
+//Space- O(n) - callback stack and dp array
