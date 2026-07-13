82f207d94678486154f9d87a4ba39f17887547ae
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-07-13 14:37:45 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.85%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,14 @@
 class Solution {
-    //Top-Down DP
-    //Intuition - Rob or Skip the current house
     public int rob(int[] nums) {
-        // [2,1,1,2] it is not necessary that you need to rob any one house that is adjacent - here to get max money you not rob 1 or 1 instead you go to 2 and 2
-        int dp[] = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return solve(0,nums,dp);
-    }
-
-    public int solve(int i, int nums[],int dp[]){
-        if(i >= nums.length)    return 0;
-        if(dp[i] != -1)    return dp[i];
-
-        dp[i] = Math.max(nums[i]+solve(i+2,nums,dp), solve(i+1,nums,dp));
-        return dp[i];
+        //Iterative DP - Bottom Up
+        int prev1=0,prev2=0;
+        for(int num : nums){
+            int t = prev1;
+            prev1=Math.max(num+prev2, prev1);
+            prev2=t;
+        }
+        return prev1;
     }
 }
 //Time- O(n)
-//Space- O(n) - callback stack and dp array
+//Space- O(1)
