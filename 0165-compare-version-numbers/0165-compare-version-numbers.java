df520e35708f83eef7ada029e048e3de167c73f7
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-07-26 12:00:22 +0530

    Time: 0 ms (100%), Space: 42.8 MB (77.35%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        int n1 = 0;
+        int n2 = 0;
+        int i = 0, j = 0;
+        while(i<version1.length() || j<version2.length()){
+            while(i<version1.length() && version1.charAt(i) != '.'){
+                n1 = n1*10+(version1.charAt(i)-'0');
+                i++;
+            }
+            while(j<version2.length() && version2.charAt(j) != '.'){
+                n2=n2*10+(version2.charAt(j)-'0');
+                j++;
+            }
+            if(n1 < n2) return -1;
+            if(n1 > n2) return 1;
+            n1=n2=0;
+            i++;
+            j++;
+        }
+        return 0;
+    }
+}
+//Time- O(n)
+//Space- O(1)
