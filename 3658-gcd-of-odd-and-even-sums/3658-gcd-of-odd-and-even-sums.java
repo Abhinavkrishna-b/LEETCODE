686714a29e769691eeaaf734a38206fdc112c9b0
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-07-30 15:30:43 +0530

    Time: 1 ms (76.41%), Space: 42.5 MB (59.67%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,32 +1,7 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int oddSum = 0;
-        int evenSum = 0;
-        int i=1,j=2,count=0;
-        while(count < n){
-            oddSum += i;
-            evenSum += j;
-            i+=2;
-            j+=2;
-            count++;
-        }
-
-        int gcd=1;
-        for(i=Math.min(oddSum,evenSum);i>=1 ;i--){
-            if(oddSum%i == 0 && evenSum%i == 0){
-                gcd=i;
-                break;
-            }
-        }
-        return gcd;
+        return n;
     }
 }
-//Time- O(n^2)
-//1st loop - O(n)
-//2nd loop - O(min(oddSum, evenSum))
-//How big can odd and even sums can be :
-//If you sum the first n odd numbers, oddSum becomes exactly n^2.
-//If you sum the first n even numbers, oddSum becomes exactly n(n+1) => n^2 + n.
-//So O(n) + O(n^2) ~ O(n^2)
+//Time- O(n)
 //Space- O(1)
-
