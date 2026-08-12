a6a6cccd7e0768f94a60ea443d2fa8671eec31a6
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-08-12 11:28:26 +0530

    Time: 1197 ms (5%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,15 +1,21 @@
 # Write your MySQL query statement below
+-- also refer the other submissions
 
--- MySQL is designed to be forgiving with data analysis. In MySQL, any number divided by 0 results in NULL.
-
-SELECT 
-    s.user_id, 
-    ROUND(IFNULL(SUM(IF(c.action = 'confirmed', 1, 0)) / COUNT(c.user_id), 0), 2) AS confirmation_rate
-FROM 
-    Signups s
-LEFT JOIN 
-    Confirmations c ON s.user_id = c.user_id
-GROUP BY 
-    s.user_id;
+select 
+s.user_id, 
+round(
+    sum(
+        case
+            when c.action = 'confirmed' then 1
+            else 0
+        end
+    ) / count(s.user_id)
+    ,2
+) as confirmation_rate
+from Signups as s
+left join Confirmations as c
+on s.user_id = c.user_id 
+-- here you not give c.action = 'confirmed' because you also need the non conformed row to get avg
+group by s.user_id;
 
 -- you can also use the avg - basically no of confirmed msg / total msg is avg of confirmed msg
