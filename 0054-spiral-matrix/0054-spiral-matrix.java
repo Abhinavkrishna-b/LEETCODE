da94d47eaf63237bbbbc54881b40a54fd786d644
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-08-25 15:20:53 +0530

    Time: 0 ms (100%), Space: 42.8 MB (91.13%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,44 +1,40 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        ArrayList<Integer> res = new ArrayList<>();
-        int left=0, right=n-1;
-        int top=0,bottom=m-1;
-
+        List<Integer> result = new ArrayList<>();
+        int top = 0, bottom = matrix.length-1;
+        int left =0, right = matrix[0].length-1;
         while(top <= bottom && left <= right){
-
-            //Left->right;
+            //Left to Right
             for(int i=left;i<=right;i++){
-                res.add(matrix[top][i]);
+                result.add(matrix[top][i]);
             }
             top++;
 
-            //Top->bottom
+            //Top to Bottom
             for(int i=top;i<=bottom;i++){
-                res.add(matrix[i][right]);
+                result.add(matrix[i][right]);
             }
             right--;
 
+            //Eg: the matrix has n=1 or m=1 i.e [[1,2,3]] now array out of bound will be occurs now
             if(left > right || top > bottom){
                 break;
             }
 
-            //Right->Left
+            //Right to Left
             for(int i=right;i>=left;i--){
-                res.add(matrix[bottom][i]);
+                result.add(matrix[bottom][i]);
             }
             bottom--;
 
-            //top->Bottom
+            //Bottom to Top
             for(int i=bottom;i>=top;i--){
-                res.add(matrix[i][left]);
+                result.add(matrix[i][left]);
             }
             left++;
         }
-        return res;
+        return result;
     }
 }
-//Time- O(m.n)
+//Time- O(n.m)
 //Space- O(1)
