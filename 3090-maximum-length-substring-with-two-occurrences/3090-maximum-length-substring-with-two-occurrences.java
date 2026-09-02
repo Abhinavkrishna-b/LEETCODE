91962c137003279b077ec70a7031755a88287662
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-02 11:04:37 +0530

    Time: 1 ms (100%), Space: 43.3 MB (92.48%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,25 +1,17 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        int maxLen = 0;
-        int curLen = 0;
-        int i=0,j=0;
         int freq[] = new int[26];
-
-        while(i<s.length() && j<s.length()){
-            char ch = s.charAt(j);
-
-            while(freq[ch-'a'] >= 2){
-                freq[s.charAt(i)-'a']--;
+        int i=0,j=0;
+        int maxLen = 0;
+        while(i < s.length() && j<s.length()){
+            while(freq[s.charAt(j) - 'a'] >= 2){
+                freq[s.charAt(i) - 'a']--;
                 i++;
             }
-
-            freq[ch-'a']++;
-            curLen = j-i+1;
-            maxLen = Math.max(curLen, maxLen);
+            freq[s.charAt(j) - 'a']++;
+            maxLen = Math.max((j-i+1),maxLen);
             j++;
         }
         return maxLen;
     }
 }
-//Time- O(n)
-//Space- O(26) ~ O(1)
