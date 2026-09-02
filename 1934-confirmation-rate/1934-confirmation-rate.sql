b2a851f2a9ff9085d5fb564f577a378b62d85e59
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-02 10:09:16 +0530

    Time: 679 ms (75.38%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,21 +1,9 @@
 # Write your MySQL query statement below
--- also refer the other submissions
-
-select 
-s.user_id, 
+select s.user_id,
 round(
-    sum(
-        case
-            when c.action = 'confirmed' then 1
-            else 0
-        end
-    ) / count(s.user_id)
-    ,2
-) as confirmation_rate
+    sum(if(c.action = 'confirmed',1,0)) / count(s.user_id)
+,2) as confirmation_rate
 from Signups as s
 left join Confirmations as c
-on s.user_id = c.user_id 
--- here you not give c.action = 'confirmed' because you also need the non conformed row to get avg
+on s.user_id = c.user_id
 group by s.user_id;
-
--- you can also use the avg - basically no of confirmed msg / total msg is avg of confirmed msg
