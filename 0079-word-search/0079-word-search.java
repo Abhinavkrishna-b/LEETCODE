44a0b1cd09850d4e4ab3832c8f6d631db99c086d
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-03 13:55:53 +0530

    Time: 145 ms (41.18%), Space: 42.9 MB (75.67%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int visited[][]=new int[board.length][board[0].length];
-        int index = 0;//To track the index of the word
-        for(int r=0;r<board.length;r++){
-            for(int c=0;c<board[0].length;c++){
-                if(board[r][c] == word.charAt(index)){
-                    if(search(board,word,visited,r,c,index)){
+        int visited[][] = new int[board.length][board[0].length];
+        int index = 0;
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                if(board[i][j] == word.charAt(index)){
+                    if(search(board, word, visited, i, j, index)){
                         return true;
                     }
                 }
@@ -15,31 +15,26 @@ class Solution {
     }
 
     public boolean search(char board[][], String word, int visited[][], int r, int c, int i){
-        
-        //Here we need to check for i > last index because if we check for last index we don't know if last character of matrix is equal to last character of word
-        if(i == word.length()){
-            return true;
-        }
-        if(r >= board.length || c >= board[0].length || r<0 || c<0){
-            return false;
-        }
-        if(visited[r][c] == 1){
-            return false;
-        }
-        if(word.charAt(i) != board[r][c]){
-            return false;
-        }
-        visited[r][c] = 1;
-        if(search(board,word,visited,r-1,c,i+1) || search(board,word,visited,r+1,c,i+1) || search(board,word,visited,r,c-1,i+1) || search(board,word,visited,r,c+1,i+1)){
+        if(i >= word.length()){
             return true;
         }
 
-        //We need to again reset the visited to 0 hence we are gona take a new path
-        visited[r][c] = 0;
-        return false;
+        if(r>=board.length || r<0 || c>= board[0].length || c<0 || board[r][c] != word.charAt(i)){
+            return false;
+        }
+
+        if(visited[r][c] == 1){
+            return false;
+        }
+        visited[r][c] = 1;
+        boolean result = search(board, word, visited, r, c+1, i+1) || search(board, word, visited, r, c-1, i+1) || search(board, word, visited, r+1, c, i+1) || search(board, word,visited, r-1, c, i+1);
+
+        if(!result){
+            visited[r][c] = 0;
+        }
+        return result;
     }
 }
-//Time- O(rc 4^l)
-//4^l is max no of recursive call for tot 4 directions
+//Time- O(rc . 4^l)
 //Space- O(l)
-//Where l is lenght of word
+//l is length of word
