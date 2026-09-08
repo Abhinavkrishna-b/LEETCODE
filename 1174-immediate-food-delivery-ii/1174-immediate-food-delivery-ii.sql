833a3178c5f257f34c39d51287d9039160532acb
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-08 10:11:34 +0530

    Time: 650 ms (74.41%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,13 +1,10 @@
 # Write your MySQL query statement below
-
 select 
 round(
-    sum( if(order_date = customer_pref_delivery_date,1,0) ) / count(distinct customer_id) * 100
+    count(customer_id) / (select count(distinct customer_id) from Delivery) * 100
 ,2) as immediate_percentage
-from delivery
-where (customer_id, order_date) in (
-    select  customer_id,
-    min(order_date)
+from Delivery
+where (customer_id, customer_pref_delivery_date) in 
+    (select customer_id,min(order_date)
     from Delivery
-    group by customer_id
-);
+    group by customer_id);
