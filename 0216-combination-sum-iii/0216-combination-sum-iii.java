5dd40b0deb5bc375aa60194427bad7526e84aecc
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-14 16:04:05 +0530

    Time: 0 ms (100%), Space: 42.5 MB (28.62%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -1,29 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> result = new ArrayList<>();
-        backtrack(k, n, 0, 1, 0, new ArrayList<>(), result);
+        backtrack(k, n, 1, 0, 0, new ArrayList<>(), result);
         return result;
     }
 
-    public void backtrack(int k, int n, int count, int start, int sum, List<Integer> temp, List<List<Integer>> result){
-        if(count == k && count != 0){
+    public void backtrack(int k, int n, int start, int sum, int count, List<Integer> temp, List<List<Integer>> result){
+        if(k != 0 && k == count){
             if(sum == n){
                 result.add(new ArrayList<>(temp));
             }
-            //else
             return;
         }
         for(int i=start;i<=9;i++){
             temp.add(i);
             sum+=i;
             count++;
-            backtrack(k, n, count, i+1, sum, temp, result);
+            backtrack(k, n, i+1, sum, count, temp,result);
             temp.remove(temp.size()-1);
             sum-=i;
             count--;
         }
     }
 }
-//Time- O(9^k)
+//Time- O(9 . 9^k) where k is the depth of the tree
 //Space- O(n)
-//Note i pass the start initially as 1 and i intialized for start and again recursion happens for i+1
