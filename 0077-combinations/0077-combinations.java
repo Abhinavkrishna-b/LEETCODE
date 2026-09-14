93bd64267906171d20cdb229746451e2683a8cec
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-14 13:07:03 +0530

    Time: 15 ms (92.28%), Space: 98.2 MB (12.98%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,6 +2,7 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
         backtrack(n, k, 1, 0, new ArrayList<>(), result);
+        //Here pass start = 1 as n starts from 1 not 0
         return result;
     }
 
@@ -10,14 +11,14 @@ class Solution {
             result.add(new ArrayList<>(temp));
             return;
         }
-        for(int i=start;i<=n;i++){
+        for(int i=start; i<=n;i++){
             temp.add(i);
             count++;
-            backtrack(n, k, i+1, count, temp, result);
-            count--;
+            backtrack(n, k, i+1, count,temp, result);
             temp.remove(temp.size()-1);
+            count--;
         }
     }
 }
-//Time- O(n^k)
+//Time- O(n^k) see the decision tree you drawn and you will understand
 //Space- O(n)
