bc1581823b50d045c9493c792c8a5cc552347d3e
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-20 12:50:46 +0530

    Time: 418 ms (42.04%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
 select query_name,
-round(sum(rating / position) / count(query_name),2) as quality,
 round(
-    sum( if(rating < 3 , 1, 0) ) / count(query_name) * 100 
+    sum(rating/position) / count(query_name)
+,2) as quality,
+round(
+    (sum(if(rating < 3,1,0)) / count(query_name)) * 100
 ,2) as poor_query_percentage
 from Queries
 group by query_name;
