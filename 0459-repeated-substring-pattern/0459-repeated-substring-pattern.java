27acd33457ae88c37c9e1339a4d42bf18fbbe49d
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-22 14:32:25 +0530

    Time: 8 ms (89.3%), Space: 47.1 MB (9.56%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        for(int i=1;i<=n/2;i++){
+            if(n%i == 0){
+                String temp = s.substring(0,i).repeat(n/i);
+                if(temp.equals(s)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+//Time- O(n^2)
+//Space- O(1)
+//As here i created a string temp without it also i can directly check that in the if() so it is O(1) space i considered
