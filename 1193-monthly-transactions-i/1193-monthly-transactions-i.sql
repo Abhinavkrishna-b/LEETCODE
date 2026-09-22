1c19a9dc294c621172c1ac0e92916a627c777b19
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-22 09:52:26 +0530

    Time: 581 ms (87.78%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,6 +1,4 @@
 # Write your MySQL query statement below
--- Use the LEFT function to get the YYYY-MM part from the trans_date. The LEFT() function extracts a number of characters from a string (starting from left).
-
 select left(trans_date,7) as month,
 country,
 count(id) as trans_count,
@@ -8,4 +6,4 @@ sum(if(state = 'approved',1,0)) as approved_count,
 sum(amount) as trans_total_amount,
 sum(if(state = 'approved',amount,0)) as approved_total_amount
 from Transactions
-group by month, country;
+group by country, left(trans_date,7)
