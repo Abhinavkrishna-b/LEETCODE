bcc5aa0ec5cb26af9cbcf1d21058ddb6ac7e508d
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-23 15:08:13 +0530

    Time: 2 ms (10.65%), Space: 43.9 MB (16.61%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(Arrays.asList(1));
+        for(int i=0;i<numRows-1;i++){
+
+            //Getting the last row values from result
+            List<Integer> prev = result.get(result.size()-1);
+            
+            //A new temp list with the first and last added extra zero
+            List<Integer> temp = new ArrayList<>();
+            temp.add(0);
+            //copy the prev val in the middle of zeros
+            for(int j=0;j<prev.size();j++){
+                temp.add(prev.get(j));
+            }
+            temp.add(0);
+
+            //Here put the running sum of the temp into list
+            List<Integer> curRow = new ArrayList<>();
+            for(int k=0;k<temp.size()-1;k++){
+                curRow.add(temp.get(k) + temp.get(k+1));
+            }
+            result.add(curRow);
+        }
+        return result;
+    }
+}
