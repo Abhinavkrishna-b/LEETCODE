b1c8a530751c0cc3a03289d96b32e151165845dd
Abhinav Krishna B <abhinavkrishnab.23cse@kongu.edu>
2026-09-24 16:23:47 +0530

    Time: 3 ms (22.7%), Space: 43.2 MB (28.85%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,16 +1,21 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        // We Had only a fixed sliding window of 3
-        //So it is easy to just trace that by i,i+1, i+2 to check if the window is unique
-        //Or else you can also use a hashset as its space complexity is O(3) ~ O(1)
-
-        int i=0;
+        int i=0, j=0;
         int count = 0;
-        for(i=0;i<s.length()-2;i++){
-            // a != b && a != c && b != c
-            if(s.charAt(i) != s.charAt(i+1) && s.charAt(i) != s.charAt(i+2) && s.charAt(i+1) != s.charAt(i+2)){
-                count++;
+        HashSet<Character> window = new HashSet<>();
+        while(i<s.length() && j<s.length()){
+            while(window.contains(s.charAt(j))){
+                window.remove(s.charAt(i));
+                i++;
             }
+            window.add(s.charAt(j));
+            if(window.size() == 3){
+                count++;
+                //Important After finding the valid window you need to remove the first element
+                window.remove(s.charAt(i));
+                i++;
+            }
+            j++;
         }
         return count;
     }
